c5b13e40a08bae390d9229000609348df9be0a52
witspirit <witspirit@gmail.com>
2016-12-04 14:15:27 +0100

    Basic SSE setup working.
    No proper design in place yet. Just discovering how this thing works.

--- src/main/java/be/witspirit/sse/ui/HomeUi.java
+++ src/main/java/be/witspirit/sse/ui/HomeUi.java
@@ -1,13 +1,21 @@
 package be.witspirit.sse.ui;
 
+import be.witspirit.sse.api.EventApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
 
 /**
  * Skeleton UI
  */
 @Controller
 public class HomeUi {
+
+    @Autowired
+    private EventApi eventApi;
 
     @RequestMapping("/")
     public String rootRedirect() {
@@ -17,5 +25,23 @@ public class HomeUi {
     @RequestMapping({"/home*", "/index*"})
     public String homePage() {
         return "home";
+    }
+
+    @RequestMapping("/publish.html")
+    public String publish() {
+        return "publish";
+    }
+
+    @RequestMapping("/viewEvents.html")
+    public String viewEvents() {
+        return "viewEvents";
+    }
+
+
+
+    @RequestMapping("publishMessage")
+    public String publishMessage(@RequestParam("destination") String destination, @RequestParam("message") String message) throws IOException {
+        eventApi.deliver(destination, message);
+        return "redirect:/publish.html";
     }
 }
